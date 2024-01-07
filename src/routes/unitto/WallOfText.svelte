5c387d0ac9642f74995b07543759718c4b1bd9e9
Sad Ellie <sadellie.dev@gmail.com>
2024-01-07 19:48:14 +0300

    Wall of text

---
+++ src/routes/unitto/WallOfText.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Footer from "./Footer.svelte";
+</script>
+
+<div class="flex flex-col gap-8 p-4 w-full">
+  <div class="flex flex-col gap-4">
+    <slot />
+  </div>
+  <hr />
+  <Footer />
+</div>
