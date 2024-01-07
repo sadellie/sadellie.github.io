a50e5f86344fec0f42de736aa0b0431f573ba750
Sad Ellie <sadellie.dev@gmail.com>
2024-01-07 19:40:43 +0300

    Use components for texts

---
+++ src/routes/unitto/TextHeader1.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<p class="text-2xl font-medium">
+    <slot></slot>
+</p>
---
+++ src/routes/unitto/TextTitle.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<p class="text-3xl font-bold">
+    <slot></slot>
+</p>
