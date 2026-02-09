f9f3da4f868d30f4ad42ad4e66c98b67bd87e083
Sad Ellie <sadellie.dev@gmail.com>
2026-02-09 19:54:30 +0300

    fix adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				return;
-			}
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    },
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        return;
+      },
+    },
+  },
 };
 
 export default config;
